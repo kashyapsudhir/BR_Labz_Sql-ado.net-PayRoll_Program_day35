1331a5bfc2bac0c0635a5ff6cc6312e8cdc535d2
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-06-12 18:40:07 +0530

    [create database and use database]

---
+++ PayRoll_Program33/PayRoll_Program33/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PayRoll_Program33
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello Day-33 Program Address Payroll Program\n");
+
+            Console.WriteLine("create database payroll_service;\r\nshow database;\r\nuse payroll_service;");
+        }
+    }
+}
---
+++ SQL-Program.sql
@@ -0,0 +1,6 @@
+--Hello Address Book Program Day-33 Program--
+
+
+create database payroll_service;
+show database;
+use payroll_service;
